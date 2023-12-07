6a3c898ed4edaf9f9499b89cebccad38d51e0574
affaqkhan211 <affaqkhan211@gmail.com>
2023-12-07 20:35:48 +0500

    home page completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import TeacherSignup from './pages/authentication/teacher/TeacherSignup';
+import TeacherSignin from './pages/authentication/teacher/TeacherSignin';
+import DeoSignup from './pages/authentication/DEO/DeoSignup';
+import DeoSignin from './pages/authentication/DEO/DeoSignin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path = "/teacher-signup" element = {<TeacherSignup/>}/>
+      <Route path = "/teacher-signin" element = {<TeacherSignin/>}/>
+      <Route path = "/deo-signup" element = {<DeoSignup/>}/>
+      <Route path = "/deo-signin" element = {<DeoSignin/>}/>
+    </Routes>
+    </>
   );
 }
 
